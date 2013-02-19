00a93ebab27371aa071803d24be54339b08dba2c
mohuzi <113357157@qq.com>
2013-02-19 15:43:54 -0500

    10% completed

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 CS447Project
 ============
+
+How to use:
+
+1. Drag the proj-skeleton directory to ecelinux machine root directory. (cd ~/)
+2. cd proj-skeleton
+3. chmod u+rwx *
+4. ./RUN
+
+
+pipair.cpp is the code we are working on.
+For more information about the code, reference to the pipair.h .
+
+P.S  Please only update the pipair.cpp and pipair.h.
+
---
+++ proj-skeleton/pipair.cpp
@@ -0,0 +1,126 @@
+#include <iostream>
+#include <unistd.h> // for system calls
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+unsigned int T_SUPPORT = 3; // default support
+unsigned int T_CONFIDENCE = 65; // default confidence
+
+enum 
+{
+  PIPE_READ = 0,
+  PIPE_WRITE,
+};
+
+
+
+
+void parser(std::vector<string> &callgraph){
+
+
+
+}
+
+void statistic(){
+  
+}
+
+int callgraph_gen( char* argv, vector<string> &callgraph ) {
+
+	string line;
+	/* pipe to connect opt's stderr and our stdin */
+  int pipe_callgraph[2];
+
+  /* pid of opt */
+  int opt_pid;
+
+  /* arguments */
+  char *bc_file;
+  int support;
+  double confidence;
+
+  /* check arguments */
+  /* !!!parse arguments yourself!!! */
+  bc_file = argv;
+
+  /* create pipe and check if pipe succeeded */
+  if (pipe(pipe_callgraph) < 0) {
+    cerr << "pipe" << endl;
+    return 1;
+  }
+
+  /* create child process */
+  opt_pid = fork();
+  if (!opt_pid) { /* child process, to spawn opt */
+
+    /* close the read end, since opt only write */
+    close(pipe_callgraph[PIPE_READ]);  
+
+    /* bind pipe to stderr, and check */
+    if (dup2(pipe_callgraph[PIPE_WRITE], STDERR_FILENO) < 0) {
+      cerr << "dup2 pipe_callgraph" << endl;
+      return 1;
+    }
+
+    /* print something to stderr 
+    fprintf(stderr, "This is child, just before spawning opt with %s.\n", bc_file);
+	*/
+    /* spawn opt */
+    if (execl("/usr/local/bin/opt", "opt", "-print-callgraph", bc_file, (char *)NULL) < 0) {
+      cerr << "execl opt" << endl;
+      return 1;
+    }
+
+    /* unreachable code */
+    return 0;
+  }
+
+  /* parent process */
+  
+
+  /* close the write end, since we only read */
+  close(pipe_callgraph[PIPE_WRITE]);
+
+  /* since we don't need stdin, we simply replace stdin with the pipe */
+  if (dup2(pipe_callgraph[PIPE_READ], STDIN_FILENO) < 0) {
+    cerr << "dup2 pipe_callgraph" << endl;
+    return 1;
+  }
+
+  /* we print w/e read from the pipe 
+  char c = '\0';
+  while (scanf("%c", &c) >= 1) {
+    printf("%c", c);
+  }
+	*/
+	while(getline(cin, line)) {
+		callgraph.push_back(line);
+	}
+  /* "That's all folks." */
+  return 0;
+}
+
+
+int main(int argc, char* argv[]) {
+	vector<string> callgraph;
+	switch( argc ) {
+		case 2:
+			callgraph_gen( argv[1], callgraph );
+			break;
+		case 4:
+			T_SUPPORT = atoi(argv[2]);
+			T_CONFIDENCE = atoi(argv[3]);
+			callgraph_gen( argv[1], callgraph );
+			break;
+		default:
+			cerr << "Your Command line paramaters are wrong!" << endl;
+	}; // switch
+	
+	for(int i =0; i < callgraph.size(); i++) {
+		cout << callgraph[i] << endl;
+	}
+
+}
