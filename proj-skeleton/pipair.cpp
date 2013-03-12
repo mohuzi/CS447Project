ec988b8abc17402a06a8abbe6d07983e1daed7a8
Jayanth Kottapalli <jayanth1991@hotmail.com>
2013-03-12 17:19:21 -0400

    Minor formatting changes

--- proj-skeleton/pipair.cpp
+++ proj-skeleton/pipair.cpp
@@ -14,12 +14,9 @@ map< string, int > FunctoID;
 map< int, vector<int> > FuncCalls;
 int maxID;
 
-string getFuncFromToken(const string &token) {
-  //int i;
-  //for (i = 1; token[i] != '\'' && i != token.length() -1; i++) {
-  //}
-  return token.substr(1, token.find_last_of("\'") - 1 );
-}
+string getFuncFromToken( const string &token ) {  
+  return token.substr( 1, token.find_last_of( "\'" ) - 1 );
+}// getFuncFromToken
 
 
 void parser( ) {
@@ -48,100 +45,49 @@ void parser( ) {
 		}// else if
 	}// for
 	
-	for (int i = 0; i < callgraph_tokens.size(); i++ ) {
+	for ( int i = 0; i < callgraph_tokens.size(); i++ ) {
 		if( callgraph_tokens[ i ] == "function:" ) {
 			TopLevelFunc = getFuncFromToken( callgraph_tokens[ i + 1 ] );			
 		}// if
 		else if( callgraph_tokens[ i ] == "function" && TopLevelFunc != "" ) {
 			func = getFuncFromToken( callgraph_tokens[ i +1 ] );
-            if(FuncCalls.find(FunctoID[TopLevelFunc]) != FuncCalls.end()) {
-                if( find(FuncCalls[FunctoID[TopLevelFunc]].begin(),FuncCalls[FunctoID[TopLevelFunc]].end(),FunctoID[func]) == 
-					FuncCalls[FunctoID[TopLevelFunc]].end() ){
+            if( FuncCalls.find( FunctoID[ TopLevelFunc ]) != FuncCalls.end()) {
+                if( find( FuncCalls[ FunctoID[ TopLevelFunc ] ].begin(),
+						  FuncCalls[ FunctoID[ TopLevelFunc ] ].end(),
+						  FunctoID[ func ]) == FuncCalls[ FunctoID[ TopLevelFunc ] ].end() ) {
                        
-                    FuncCalls[FunctoID[TopLevelFunc]].push_back(FunctoID[func]);
-                }
+                    FuncCalls[ FunctoID[ TopLevelFunc ] ].push_back( FunctoID[ func ] );
+                }//if
             }// if
             else {                 
-				FuncCalls[FunctoID[TopLevelFunc]].push_back(FunctoID[func]);
+				FuncCalls[ FunctoID[ TopLevelFunc ] ].push_back( FunctoID[ func ] );
             }// else
 		}// else if
 	}// for
-	maxID = ID - 1;
-    /*for (list<string>::iterator it=tokens.begin(); it != tokens.end(); ++it){
-         if (*it == "function:"){
-              ++it;
-              startFlag = true;
-              if(FunctoID.find(getFuncfromToken(*it)) == FunctoID.end()){
-                  //cout << getFuncfromToken(*it)<<endl;
-                  string func = getFuncfromToken(*it);
-                  FunctoID[func] = ID;
-                  IDtoFunc[ID] =func;
-                  ID++;
-              }
-              --it;
-         }
-         if (*it == "function" && startFlag){
-              ++it;
-              
-              if(FunctoID.find(getFuncfromToken(*it)) == FunctoID.end()){
-                  //cout << getFuncfromToken(*it)<<endl;
-                  string func = getFuncfromToken(*it);
-                  FunctoID[func] = ID;
-                  IDtoFunc[ID] =func;
-                  ID++;
-              }
-              --it;
-         }           
-    }        
-	for (list<string>::iterator it=tokens.begin(); it != tokens.end(); ++it){
-        if (*it == "function:"){
-              ++it;
-              string func = getFuncfromToken(*it);
-              TopLevelFunc = func;
-              --it;
-         }
-         if (*it == "function" && TopLevelFunc != ""){
-              ++it;
-              string func = getFuncfromToken(*it);
-
-              if(FuncCalls.find(FunctoID[TopLevelFunc]) != FuncCalls.end()) {
-                   if( find(FuncCalls[FunctoID[TopLevelFunc]].begin(),FuncCalls[FunctoID[TopLevelFunc]].end(),FunctoID[func]) == FuncCalls[FunctoID[TopLevelFunc]].end() ){
-                       
-                      FuncCalls[FunctoID[TopLevelFunc]].push_back(FunctoID[func]);
-                   }
-              }
-              else{
-                 
-                FuncCalls[FunctoID[TopLevelFunc]].push_back(FunctoID[func]);
-              }
-              --it;
-         }
-    }
-    maxID = ID -1; 
-	*/
+	maxID = ID - 1;    
 }// parser
 
 
 // Using the parse data, calculate the support for functions and function pairs, and then return the function pairs which we have inferred must always occur together
-void analyze( vector<map<int,FunctionPair> > &Pairs ) {
+void analyze( vector< map< int, FunctionPair > > &Pairs ) {
 
-  cout << "Beginning Analysis" << flush << endl; //? what is flush?
-  vector<int> support = vector<int>(maxID,0);
+	cout << "Beginning Analysis" << flush << endl; //? what is flush?
+	vector< int > support = vector< int >( maxID, 0 );
 	int a = 0, b = 0;
 
 	//Calculate support for each function and function pair
-	for( map< int, vector<int> >::iterator i = FuncCalls.begin(); i != FuncCalls.end(); ++i ) {
+	for( map< int, vector< int > >::iterator i = FuncCalls.begin(); i != FuncCalls.end(); ++i ) {
 
-		vector<int> &v = i->second;
+		vector< int > &v = i->second;
 		//Remove duplicate function calls (maybe should do in the parser?)
 		sort( v.begin(), v.end() );
 		v.erase( unique( v.begin(), v.end() ), v.end() );
 
 		//Go through all function pairs
-		for(int j = 0; j < v.size(); j++ ) {
+		for( int j = 0; j < v.size(); j++ ) {
 			a = v[ j ];
-			support[a]++;
-			for(int k = 0; k < v.size(); k++ ){				
+			support[ a ]++;
+			for( int k = 0; k < v.size(); k++ ){				
 				b = v[ k ];
 				if ( a != b ) {					
 					Pairs[ a ][ b ].support++;
@@ -154,19 +100,20 @@ void analyze( vector<map<int,FunctionPair> > &Pairs ) {
 
 	//Calculate confidence for each function pair, and throw out any pairs that don't meet the threasholds
 	//Loop through all function pairs
-    for (int i = 0; i < Pairs.size(); i++ ){
-		for (map<int,FunctionPair>::iterator j = Pairs[i].begin(); j != Pairs[i].end(); ++j){
+    for ( int i = 0; i < Pairs.size(); i++ ){
+		for ( map< int, FunctionPair >::iterator j = Pairs[ i ].begin(); j != Pairs[ i ].end(); ++j ) {
 			FunctionPair &p = j->second;
 			//cout << p.a << " " << p.b << " " << p.support << " " << (p.support) * 100 / support[p.a] << endl;
-			if( p.support < T_SUPPORT || (p.support) * 10000 / support[p.a] < T_CONFIDENCE){
+			if( p.support < T_SUPPORT || (p.support) * 10000 / support[ p.a ] < T_CONFIDENCE){
 				//Doesn't meet support or confidence threasholds
-				Pairs[i].erase(j);
-			} else {
+				Pairs[ i ].erase( j );
+			} //if
+			else {
 				//Does meet thresholds, keep and record confidence
-				p.confidence = (p.support) * 10000 / support[p.a];
-			}
-		}
-	}
+				p.confidence = ( p.support ) * 10000 / support[ p.a ];
+			}//else
+		}//for
+	}//for
 	cout << "Ending Analysis" << flush << endl;
 }//  analyze
 
@@ -176,7 +123,7 @@ void find_bugs() {
 void callgraph_gen( char* argv ) {
 
 	/* pipe to connect opt's stderr and our stdin */
-	int pipe_callgraph[2];
+	int pipe_callgraph[ 2 ];
 
 	/* pid of opt */
 	int opt_pid;
@@ -191,56 +138,56 @@ void callgraph_gen( char* argv ) {
 	bc_file = argv;
 
 	/* create pipe and check if pipe succeeded */
-	if (pipe(pipe_callgraph) < 0) {
+	if ( pipe( pipe_callgraph ) < 0 ) {
 		cerr << "pipe" << endl;	
-	}
+	}//if
 
 	/* create child process */
 	opt_pid = fork();
-	if (!opt_pid) { /* child process, to spawn opt */
+	if ( !opt_pid ) { /* child process, to spawn opt */
 
 		/* close the read end, since opt only write */
-		close(pipe_callgraph[PIPE_READ]);  
+		close( pipe_callgraph[ PIPE_READ ] );  
 
 		/* bind pipe to stderr, and check */
-		if (dup2(pipe_callgraph[PIPE_WRITE], STDERR_FILENO) < 0) {
+		if ( dup2( pipe_callgraph[ PIPE_WRITE ], STDERR_FILENO ) < 0) {
 			cerr << "dup2 pipe_callgraph" << endl;			
-		}
+		}//if
 
 		//redirect the stdout to a dummy pipe
-		int dummyPipe[2];
-		pipe(dummyPipe);
-		dup2(dummyPipe[1],1);
+		int dummyPipe[ 2 ];
+		pipe( dummyPipe );
+		dup2( dummyPipe[ 1 ], 1);
      
 
 		/* print something to stderr 
 		fprintf(stderr, "This is child, just before spawning opt with %s.\n", bc_file);
 		*/
 		/* spawn opt */
-		if (execl("/usr/local/bin/opt", "opt", "-print-callgraph", bc_file, (char *)NULL) < 0) {
+		if ( execl( "/usr/local/bin/opt", "opt", "-print-callgraph", bc_file, (char *)NULL ) < 0 ) {
 			cerr << "execl opt" << endl;			
 		}
      
-		close(dummyPipe[1]);
-		close(dummyPipe[0]);
+		close( dummyPipe[ 1 ] );
+		close( dummyPipe[ 0 ] );
 	}// if
 
 	/* parent process */
   
 
 	/* close the write end, since we only read */
-	close(pipe_callgraph[PIPE_WRITE]);
+	close( pipe_callgraph[ PIPE_WRITE ] );
 
 	/* since we don't need stdin, we simply replace stdin with the pipe */
-	if (dup2(pipe_callgraph[PIPE_READ], STDIN_FILENO) < 0) {
+	if ( dup2( pipe_callgraph[ PIPE_READ ], STDIN_FILENO ) < 0 ) {
 		cerr << "dup2 pipe_callgraph" << endl;	
-	}
+	}//if
 
 	string line;
 	string token="";
 	//mark 1
 	while( cin >> token ) {
-		callgraph_tokens.push_back(token);
+		callgraph_tokens.push_back( token );
 		//tokens.push_back( token );
 	}// while
 
@@ -256,11 +203,11 @@ void callgraph_gen( char* argv ) {
 int main(int argc, char* argv[]) {	
 	switch( argc ) {		
 		case 4:
-			T_SUPPORT = atoi(argv[2]);
-			T_CONFIDENCE = atoi(argv[3]);
+			T_SUPPORT = atoi( argv[ 2 ] );
+			T_CONFIDENCE = atoi( argv[ 3 ] );
 			// FALL THROUGH
 		case 2:
-			callgraph_gen( argv[1] );
+			callgraph_gen( argv[ 1 ] );
 			break;
 		default:
 			cerr << "Your Command line paramaters are wrong!" << endl;
@@ -271,34 +218,34 @@ int main(int argc, char* argv[]) {
 	// To see what we have in those data structure. 
   
 	cout << "function map :" << endl;
-	for (map<int, string>::iterator  it=IDtoFunc.begin(); it!=IDtoFunc.end(); ++it)
+	for ( map< int, string >::iterator  it = IDtoFunc.begin(); it != IDtoFunc.end(); ++it )
 		cout << it->first << " => " << it->second << '\n';
 
 	cout << endl<<"Call functions :" << endl;
-	for (map<int, vector<int> >::iterator  it=FuncCalls.begin(); it!=FuncCalls.end(); ++it){
+	for ( map< int, vector < int > >::iterator  it = FuncCalls.begin(); it != FuncCalls.end(); ++it ){
 		cout << it->first << " calls:" << endl;
-		for (int it2 = 0; it2 < it->second.size(); it2++){
+		for ( int it2 = 0; it2 < it->second.size(); it2++ ){
 			cout << it->second[ it2 ] <<" ";
 		}
 		cout << endl;
 	}  	
 
 	// Your Code here:
-	vector<map<int,FunctionPair> > Pairs(maxID + 1);
+	vector< map< int, FunctionPair > > Pairs( maxID + 1 );
 	analyze( Pairs );  
 	cout << "Learned Constraints :" << flush << endl;	
 	//Loop through all function pairs
-	for(int i = 0; i < Pairs.size(); i++ ){
-		for(map<int,FunctionPair>::iterator j= Pairs[i].begin(); j != Pairs[i].end(); ++j){
+	for( int i = 0; i < Pairs.size(); i++ ){
+		for( map< int, FunctionPair >::iterator j = Pairs[ i ].begin(); j != Pairs[ i ].end(); ++j ) {
 			FunctionPair &p = j->second;
-			cout << "pair: (" << IDtoFunc[p.a] << "," << IDtoFunc[p.b] << "), support:" << p.support << ", condfidence:" << p.confidence << endl;
+			cout << "pair: (" << IDtoFunc[ p.a ] << "," << IDtoFunc[ p.b ] << "), support:" << p.support << ", condfidence:" << p.confidence << endl;
 		}
 	}
 
 	//To see the original Bitcode. To use this, u need to comment out line 175 to line 179 (mark 1 to mark 1 end)
-	for(int i =0; i < callgraph.size(); i++)
+	for( int i =0; i < callgraph.size(); i++ )
 	{		
-		cout << callgraph[i] << endl;
+		cout << callgraph[ i ] << endl;
 	}
 
 }
